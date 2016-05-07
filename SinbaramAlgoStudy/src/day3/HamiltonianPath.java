75bf5cef0cc758d1cb08838228b1339a88592cd3
Administrator <Administrator@user>
2016-05-07 14:20:51 +0900

    TLE

--- SinbaramAlgoStudy/src/day3/HamiltonianPath.java
+++ SinbaramAlgoStudy/src/day3/HamiltonianPath.java
@@ -26,31 +26,29 @@ public class HamiltonianPath {
 	private static int getMinCost(int n){
 		visited[n] = true;
 		boolean[] mVisited = new boolean[visited.length];
+		boolean[] minVisited = new boolean[visited.length];
+		boolean isFinal = true;
 		copyArray(visited, mVisited);
 		int min = Integer.MAX_VALUE;
-		int idx = n;
 		for(int i=0; i<costs[n].length; ++i){
-			if(i != n && !visited[i] && costs[n][i] != 0){
-				int temp = costs[n][i]+getMinCost(i);
-				if(min > temp){
-					min = temp;
-					idx = i;
+			if(!visited[i] && n!=i){
+				isFinal = false;
+				if(costs[n][i] != 0 && canComeback(n, visited)){
+					int temp = costs[n][i]+getMinCost(i);
+					if(min > temp){
+						min = temp;
+						copyArray(visited, minVisited);
+					}
+					copyArray(mVisited, visited);
 				}
-				copyArray(mVisited, visited);
 			}
 		}
 		// 모두 다 가봤으면 회사로 돌아가렴
-		if(min == Integer.MAX_VALUE){
-			if(costs[n][0] != 0){
-				min = costs[n][0];
-			}
-			// 못가는 길이면 선택 못하게 최대값 박아버림 -_-;;
-			else{
-				visited[n]= false;
-				return 1200;
-			}
-		}else{
-			visited[idx] = true;
+		if(isFinal){
+			min = costs[n][0];
+		}
+		else{
+			copyArray(minVisited, visited);
 		}
 		return min;
 	}
@@ -66,6 +64,16 @@ public class HamiltonianPath {
 //		}
 //	}
 	
+	private static boolean canComeback(int n, boolean[] visited) {
+		
+		for(int i=0; i<visited.length; ++i){
+			if(i!=n && !visited[i] && costs[i][0] != 0){
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private static void copyArray(boolean[] source, boolean[] target){
 		for(int i=0; i<source.length; ++i){
 			target[i] = source[i];
