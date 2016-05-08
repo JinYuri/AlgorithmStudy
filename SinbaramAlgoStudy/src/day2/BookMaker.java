f8ae5ffb44a4ef8a5801e76894b097f82fdf050b
Administrator <Administrator@user>
2016-05-08 12:58:29 +0900

    배열로 다시 변환(sort필요없어)

--- SinbaramAlgoStudy/src/day2/BookMaker.java
+++ SinbaramAlgoStudy/src/day2/BookMaker.java
@@ -1,9 +1,6 @@
 package day2;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -14,29 +11,29 @@ public class BookMaker {
 		int t = sc.nextInt();
 		for(int tc=0; tc<t; ++tc){
 			int k = sc.nextInt();
-			List<Integer> files = new ArrayList<>();
+			int[] files = new int[k];
 			mem = new HashMap<>();
 			
 			for(int i=0; i<k; ++i){
-				files.add(sc.nextInt());
+				files[i] = sc.nextInt();
 			}
 			System.out.println(getMinCost(files));
 		}
 		sc.close();
 	}
 	
-	private static int getMinCost(List<Integer> files){
+	private static int getMinCost(int[] files){
 		String key = makeKey(files);
 		if(mem.containsKey(key)){
 			return mem.get(key);
 		}
 		int min = Integer.MAX_VALUE;
-		if(files.size() == 2){
-			min = files.get(0) + files.get(1);
+		if(files.length == 2){
+			min = files[0] + files[1];
 		}
 		else{
-			for(int i=0; i<files.size()-1; ++i){
-				int cost = files.get(i) + files.get(i+1);
+			for(int i=0; i<files.length-1; ++i){
+				int cost = files[i] + files[i+1];
 				int temp = cost + getMinCost(makeNewFile(files, i, cost));
 				if(min > temp){
 					min = temp;
@@ -47,29 +44,26 @@ public class BookMaker {
 		return min;
 	}
 	
-	private static List<Integer> makeNewFile(List<Integer> source, int idx, int cost){
-		List<Integer> target = new ArrayList<>();
-		for(int i=0; i<source.size()-1; ++i){
+	private static int[] makeNewFile(int[] source, int idx, int cost){
+		int[] target = new int[source.length-1];
+		for(int i=0; i<source.length-1; ++i){
 			if(i == idx){
-				target.add(cost);
+				target[i] = cost;
 			}
 			else if(i < idx){
-				target.add(source.get(i));
+				target[i] = source[i];
 			}
 			else{
-				target.add(source.get(i+1));
+				target[i] = source[i+1];
 			}
 		}
 		return target;
 	}
 	
-	private static String makeKey(List<Integer> files){
-		List<Integer> sorted = new ArrayList<>();
-		sorted.addAll(files);
-		Collections.sort(sorted);
+	private static String makeKey(int[] files){
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<sorted.size(); ++i){
-			sb.append(sorted.get(i)+"_");
+		for(int i=0; i<files.length; ++i){
+			sb.append(files[i]+"_");
 		}
 		return sb.toString();
 	}
