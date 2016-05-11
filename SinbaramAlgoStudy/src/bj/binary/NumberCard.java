83939e2f0897f75ff39ffa0f3d22786a00bb08f2
진유리 <yuriya.jin@samsung.com>
2016-05-11 20:16:18 +0900

    number card TLE

--- SinbaramAlgoStudy/src/bj/binary/NumberCard.java
+++ SinbaramAlgoStudy/src/bj/binary/NumberCard.java
@@ -2,11 +2,15 @@ package bj.binary;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-//https://www.acmicpc.net/problem/10815
+
 public class NumberCard {
 	private static List<Integer> cards = new ArrayList<>();
+	private static Map<Integer, Integer> mem = new HashMap<>();
+	private static Map<Integer, Integer> iMap = new HashMap<>();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -26,17 +30,45 @@ public class NumberCard {
 	}
 	
 	private static int findCard(int m, int start, int end){
+		if(mem.containsKey(m)){
+			return mem.get(m);
+		}
 		if(start>end){
+			mem.put(m, 0);
 			return 0;
 		}
-		if(cards.get((start+end)/2) == m){
+		int idx = (start+end)/2;
+		int temp = cards.get(idx);
+		mem.put(temp, 1);
+		
+		int iIdx = temp/5;
+		if(iMap.containsKey(iIdx)){
+			int tempStart = iMap.get(iIdx);
+			if(tempStart < start){
+				iMap.put(iIdx, start);
+			}
+			else{
+				start = tempStart;
+			}
+		}else{
+			iMap.put(iIdx, start);
+		}
+		
+		if(iMap.containsKey(iIdx+1)){
+			int tempEnd = iMap.get(iIdx+1);
+			if(tempEnd < end && start < tempEnd){
+				end = tempEnd;
+			}
+		}
+		
+		if(temp == m){
 			return 1;
 		}
-		else if(cards.get((start+end)/2) > m){
-			return findCard(m, start, ((start+end)/2)-1);
+		else if(temp > m){
+			return findCard(m, start, idx-1);
 		}
 		else{
-			return findCard(m, ((start+end)/2)+1, end);
+			return findCard(m, idx+1, end);
 		}
 		
 	}
