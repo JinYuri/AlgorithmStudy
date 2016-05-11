77aa98cc2039fe1b6ffbde33a81bafb9bd6379fc
진유리 <yuriya.jin@samsung.com>
2016-05-11 21:53:11 +0900

    Buffered Reader 만세

--- SinbaramAlgoStudy/src/bj/binary/NumberCard.java
+++ SinbaramAlgoStudy/src/bj/binary/NumberCard.java
@@ -1,28 +1,30 @@
 package bj.binary;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class NumberCard {
 	private static int[] cards = null;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(new BufferedInputStream(System.in));
-		
-		int n = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		cards = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; ++i){
-			cards[i] = sc.nextInt();
+			cards[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(cards);
 		
-		int k = sc.nextInt();
+		int k = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<k; ++i){
-			sb.append(findCard(sc.nextInt(), 0, n-1)).append(" ");
+			sb.append(findCard(Integer.parseInt(st.nextToken()), 0, n-1)).append(" ");
 		}
 		System.out.print(sb.toString());
-		sc.close();
 	}
 	
 	private static int findCard(int m, int start, int end){
