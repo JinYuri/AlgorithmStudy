fde1947083cbe33b6d874f24a5e1371de104c559
진유리 <yuriya.jin@samsung.com>
2016-05-14 16:18:46 +0900

    ing tree 인접리스트 TLE

--- SinbaramAlgoStudy/src/bj/camp/TreeWeight.java
+++ SinbaramAlgoStudy/src/bj/camp/TreeWeight.java
@@ -4,29 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class TreeWeight {
 	private static int N = Integer.MIN_VALUE;
-	private static int[][] map = null;
+	private static Vector<Integer>[] map = null;
 	private static int[] visited = null;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		visited = new int[N];
-		map = new int[N][N];
+		map = new Vector[N];
 		StringTokenizer st = null;
 		
-		//인접행렬만들기
+		//인접리스트만들기
 		for(int i=1; i<N; ++i){
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken())-1;
 			int m = Integer.parseInt(st.nextToken())-1;
+			int w = Integer.parseInt(st.nextToken());
+			if(map[n] == null){
+				map[n] = new Vector<>(1, 1);
+			}
+			if(map[m] == null){
+				map[m] = new Vector<>(1, 1);
+			}
+			map[n].add(m);
+			map[n].add(w);
+			map[m].add(n);
+			map[m].add(w);
 			
-			map[n][m] = map[m][n] = Integer.parseInt(st.nextToken());
 		}
-		
 		// 모든경로 구하기
 		// 경로의 가중치 (엣지의 곱)를 구하고 이를 sum하여 return
 		List<Integer> result = getWieght(0);
@@ -40,15 +52,17 @@ public class TreeWeight {
     public static List<Integer> getWieght(int n){
         List<Integer> result = new ArrayList<>();
         visited[n] = 1;
-        for(int i=0; i<N; i++){
-            if(map[n][i] > 0 && visited[i] != 1){
-            	result.add(map[n][i]);
-            	List<Integer> temp = getWieght(i);
-            	for(int val : temp){
-            		result.add(val);
-            		result.add((map[n][i]*val)%1000000007);
-            	}
-            }
+        if(map[n] != null){
+        	for(int i=0; i<map[n].size(); i+=2){
+        		if(visited[map[n].get(i)] != 1){
+        			result.add(map[n].get(i+1));
+        			List<Integer> temp = getWieght(map[n].get(i));
+        			for(int val : temp){
+        				result.add(val);
+        				result.add((map[n].get(i+1)*val)%1000000007);
+        			}
+        		}
+        	}
         }
         return result;
     }
