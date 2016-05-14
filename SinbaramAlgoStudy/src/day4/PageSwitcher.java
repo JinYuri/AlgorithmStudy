248e4cecf9a4f84bbf6a45be5e2a03b0264e2f5e
진유리 <yuriya.jin@samsung.com>
2016-05-14 17:06:44 +0900

    start pageSwitcher!

--- SinbaramAlgoStudy/src/day4/PageSwitcher.java
+++ SinbaramAlgoStudy/src/day4/PageSwitcher.java
@@ -1,5 +1,7 @@
 package day4;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -8,19 +10,27 @@ public class PageSwitcher {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		Vector<Integer>[] map = new Vector[500];
+		Map<Integer, Vector<Integer>> map = new HashMap<>();
 		
 		for(int i=0; i<N; ++i){
 			int from = sc.nextInt()-1;
 			int to = sc.nextInt()-1;
-			if(map[from] == null){
-				map[from] = new Vector<>(1, 1);
+			if(map.containsKey(from)){
+				map.get(from).addElement(to);
+			}else{
+				Vector<Integer> v = new Vector<>(1,1);
+				v.addElement(to);
+				map.put(from, v);
 			}
-			map[from].addElement(to);
 		}
 		
 		// ÂªÀº ÆäÀÌÁö Å¬¸¯ È½¼öÀÇ ÇÕ
 		// ½Ö °¹¼ö¸¦ ±¸ÇØ¼­ Æò±Õ Å¬¸¯ È½¼ö ±¸ÇÏ±â
+		int pairCount = N*(N-1);
 		sc.close();
+	}
+	
+	private static int getBestPath(int from, int to){
+		return 0;
 	}
 }
