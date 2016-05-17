ad0fd0e89598c68ba63ab204a40580e7e9573db6
Administrator <Administrator@user>
2016-05-17 22:22:46 +0900

    .

--- SinbaramAlgoStudy/src/day4/PageSwitcher.java
+++ SinbaramAlgoStudy/src/day4/PageSwitcher.java
@@ -6,21 +6,28 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class PageSwitcher {
+	private static Graph[] gArr = null;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		Map<Integer, Vector<Integer>> map = new HashMap<>();
-		
+//		Map<Integer, Vector<Integer>> map = new HashMap<>();
+		gArr = new Graph[N];
 		for(int i=0; i<N; ++i){
 			int from = sc.nextInt()-1;
 			int to = sc.nextInt()-1;
-			if(map.containsKey(from)){
-				map.get(from).addElement(to);
-			}else{
-				Vector<Integer> v = new Vector<>(1,1);
-				v.addElement(to);
-				map.put(from, v);
+//			if(map.containsKey(from)){
+//				map.get(from).addElement(to);
+//			}else{
+//				Vector<Integer> v = new Vector<>(1,1);
+//				v.addElement(to);
+//				map.put(from, v);
+//			}
+			if(gArr[i] == null){
+				gArr[i] = new Graph();
+			}
+			if(gArr[from] == null){
+				gArr[from].map.put(to, 1);
 			}
 		}
 		
@@ -30,7 +37,14 @@ public class PageSwitcher {
 		sc.close();
 	}
 	
-	private static int getBestPath(int from, int to){
+	private static int getBestPath(int from){
+		if(gArr[from].map.containsKey(from)){
+			
+		}
 		return 0;
 	}
+	public static class Graph{
+		Map<Integer, Integer> map = new HashMap<>();
+	}
 }
+
