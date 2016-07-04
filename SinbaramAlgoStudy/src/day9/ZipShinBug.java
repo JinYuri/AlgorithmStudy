b21553129928e1a1bb6499f5e949f162d59a945f
진유리 <yuriya.jin@samsung.com>
2016-07-04 13:35:47 +0900

    stack overflow..

--- SinbaramAlgoStudy/src/day9/ZipShinBug.java
+++ SinbaramAlgoStudy/src/day9/ZipShinBug.java
@@ -14,41 +14,43 @@ public class ZipShinBug {
 		d = sc.nextInt(); //태어나고 d일 후 사망 ㅠㅠ
 		int N = sc.nextInt(); //구하고자 하는 일자
 		
-		System.out.println(getCount(1, N));
+		System.out.println(getCount(1, N)%1000);
 		sc.close();
 	}
 	
 	private static int getCount(int aliveDays, int leftDays){
+		int result = 0;
 		// 성숙하지 않음
 		if(aliveDays < a){
 			// 구하는 날까지 성숙하지 않으면
-			if(leftDays < a){
-				return 1;
+			if(leftDays <= a){
+				result = 1;
 			}
 			// 성숙하는 날로 점프
 			else{
-				return getCount(a, leftDays-a+aliveDays);
+				result = getCount(a, leftDays-a+aliveDays)%1000;
 			}
 		}
 		//복제 그만
 		else if(aliveDays >= b){
 			// 구하는 날까지 살아 있다면
 			if(leftDays <= d-aliveDays){
-				return 1;
+				result = 1;
 			}
 			else{
-				return 0;
+				result = 0;
 			}	
 		}
 		// 성숙해서 복제 가능
 		else{
-			// 그날이 왔다
-			if(leftDays <= 0){
-				return 1;
-			} 
+			// 오늘이 마지막이라면
+			if(leftDays <= 1){
+				result = 2;
+			}
 			else{
-				return getCount(++aliveDays, leftDays-1)+getCount(0, leftDays-1);
+				result = getCount(aliveDays+1, leftDays-1)%1000+getCount(0, leftDays)%1000;
 			}
 		}
+		return result;
 	}
 }
